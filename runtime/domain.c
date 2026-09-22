oxcaml/oxcaml

commit f93b4ba4d2be167b27fea802e8d3c6ecf837baec

domain_terminate: orphan the shared heap before STW exit
See https://github.com/ocaml/ocaml/issues/13739#issuecomment-2869607732

runtime/domain.c
@@ -778,7 +778,9 @@ create_stack_cache_failure:
 reallocate_minor_heap_failure:
   caml_teardown_major_gc();
 init_major_gc_failure:
-  caml_teardown_shared_heap(d->state->shared_heap);
+  caml_orphan_shared_heap(d->state->shared_heap);
+  caml_free_shared_heap(d->state->shared_heap);
+  domain_state->shared_heap = NULL;
 init_shared_heap_failure:
   caml_free_minor_tables(domain_state->minor_tables);
   domain_state->minor_tables = NULL;
@@ -2072,6 +2074,11 @@ void caml_domain_terminate(bool last)
     caml_orphan_ephemerons(domain_state);
     caml_orphan_finalisers(domain_state);
 
+    if (domain_state->marking_done &&
+        domain_state->sweeping_done) {
+      caml_orphan_shared_heap(domain_state->shared_heap);
+    }
+
     /* No need to check for interrupts if we are the last domain running. */
     if (last) {
       CAML_EV_LIFECYCLE(EV_DOMAIN_TERMINATE, getpid());
@@ -2142,18 +2149,18 @@ void caml_domain_terminate(bool last)
   if (last)
     caml_finalise_heap();
 
-  caml_teardown_shared_heap(domain_state->shared_heap);
-  domain_state->shared_heap = 0;
+  caml_free_shared_heap(domain_state->shared_heap);
+  domain_state->shared_heap = NULL;
   caml_free_minor_tables(domain_state->minor_tables);
-  domain_state->minor_tables = 0;
+  domain_state->minor_tables = NULL;
 
-  caml_orphan_alloc_stats(domain_state);
-  /* Heap stats were orphaned by [caml_teardown_shared_heap] above.
-     At this point, the stats of the domain must be empty.
-
-     The sampled copy was also cleared by the minor collection(s)
-     performed above at [caml_empty_minor_heaps_once()], see the
-     termination-specific logic in [caml_collect_gc_stats_sample_stw].
+  /* At this point, the stats of the domain must be empty.
+     - heap stats were orphaned by [caml_orphan_shared_heap]
+     - alloc stats were orphaned by [caml_orphan_alloc_stats]
+     - the sampled copy in [sampled_gc_stats] was cleared by the minor
+       collection performed by [caml_empty_minor_heaps_once()], see
+       the termination-specific logic in
+       [caml_collect_gc_stats_sample_stw].
   */
 
   /* TODO: can this ever be NULL? can we remove this check? */

runtime/caml/shared_heap.h
@@ -31,7 +31,8 @@ struct caml_heap_state;
 struct pool;
 
 struct caml_heap_state* caml_init_shared_heap(void);
-void caml_teardown_shared_heap(struct caml_heap_state* heap);
+void caml_orphan_shared_heap(struct caml_heap_state* heap);
+void caml_free_shared_heap(struct caml_heap_state* heap);
 
 value* caml_shared_try_alloc(struct caml_heap_state*,
                              mlsize_t, tag_t, reserved_t);

runtime/shared_heap.c
@@ -146,7 +146,7 @@ static int move_all_pools(pool** src, _Atomic(pool*)* dst,
   return count;
 }
 
-void caml_teardown_shared_heap(struct caml_heap_state* heap) {
+void caml_orphan_shared_heap(struct caml_heap_state* heap) {
   int released = 0, released_large = 0;
 
   caml_plat_lock_blocking(&pool_freelist.lock);
@@ -173,9 +173,23 @@ void caml_teardown_shared_heap(struct caml_heap_state* heap) {
   }
   orphan_heap_stats_with_lock(heap);
   caml_plat_unlock(&pool_freelist.lock);
-  caml_stat_free(heap);
-  caml_gc_log("Shutdown shared heap. Released %d active pools, %d large",
+  caml_gc_log("Orphan shared heap. Released %d active pools, %d large",
               released, released_large);
+}
+
+void caml_free_shared_heap(struct caml_heap_state* heap) {
+  // The shared heap must have been swept,
+  // and then emptied by [caml_orphan_shared_heap].
+  for (int i = 0; i < NUM_SIZECLASSES; i++) {
+    CAMLassert(!heap->avail_pools[i]);
+    CAMLassert(!heap->full_pools[i]);
+    CAMLassert(!heap->unswept_avail_pools[i]);
+    CAMLassert(!heap->unswept_full_pools[i]);
+  }
+  CAMLassert(!heap->unswept_large);
+  CAMLassert(!heap->swept_large);
+
+  caml_stat_free(heap);
 }
 
 

edwintorok/ocaml

commit 4b8889b08fc7f10dfc056d3a222070bc0ef8b572

caml_domain_terminate: do not lose alloc stats
Fixes: f93b4ba4d2 ("domain_terminate: orphan the shared heap before STW exit")

Signed-off-by: Edwin Török <edwin@tarides.com>

runtime/domain.c
@@ -2395,6 +2395,7 @@ void caml_domain_terminate(bool last)
   caml_free_minor_tables(domain_state->minor_tables);
   domain_state->minor_tables = NULL;
 
+  caml_orphan_alloc_stats(domain_state);
   /* At this point, the stats of the domain must be empty.
      - heap stats were orphaned by [caml_orphan_shared_heap]
      - alloc stats were orphaned by [caml_orphan_alloc_stats]

testsuite/tests/lib-runtime-events/test_caml_counters_consistent.ml
@@ -142,7 +142,6 @@ let () =
     in
     Gc.full_major ();
     Gc.minor ();
-    Gc.print_stat stdout;
     if errors > 0 then begin
       Printf.eprintf "FAIL: %d mismatches\n" errors;
       exit 1
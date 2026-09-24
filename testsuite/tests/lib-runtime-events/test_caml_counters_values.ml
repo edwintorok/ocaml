bccd7b4cb00850ba92dbff0a442dda9042b3b27f
Edwin Török <edwin@tarides.com>
2026-09-24 13:48:38 +0100

    work

--- testsuite/tests/lib-runtime-events/test_caml_counters_values.ml
+++ testsuite/tests/lib-runtime-events/test_caml_counters_values.ml
@@ -20,6 +20,10 @@ type User.tag += Srcline
 let srcline = User.register "srcline" Srcline Type.int
 let line ~__LINE__ = User.write srcline __LINE__
 
+type User.tag += Allocating_words
+let allocating_words = User.register "allocating_words" Srcline Type.int
+let allocating_words value = User.write allocating_words value
+
 type User.tag += Gc_stat_observed
 let gc_stat_observed = User.register "gc_stat_observed" Gc_stat_observed Type.int
 let[@inline] gc_observed value = User.write gc_stat_observed value
@@ -97,12 +101,22 @@ module Dump = struct
   let drop () = poll cursor noop
 
 
-  let dump () =
+  let dump counter counter_type history =
+    (* avoid flooding the output with unrelated events from the allocations done by poll:
+       pause
+    *)
+    pause ();
     Format.printf "@.";
     Format.eprintf "@.%s@." (String.make 78 '=');
     Format.eprintf "@[<v>";
     poll cursor callbacks;
-    Format.eprintf "@]@."
+    Format.eprintf "@[Counter %s history:@ %a@]@," (runtime_counter_name counter)
+      Format.(pp_print_array ~pp_sep:Format.pp_print_space pp_print_int) history;
+    Format.eprintf "@]@.";
+    resume ();
+    Gc.minor ();
+    drop ()
+
 end
 
 let lost_events_fail domain_id lost_words=
@@ -113,12 +127,17 @@ type counter_type = Additive | Absolute
 
 let run_and_compare ~__LINE__ f counter_expected counter_type observe_gc =
   let counter_value = ref 0 in
+  let counter_history = Array.make 10_000 0 in
+  let history = ref 0 in
   let runtime_counter domain_id timestamp counter value =
-    if counter = counter_expected then
+    if counter = counter_expected then begin
       if counter_type = Additive then
         counter_value := !counter_value + value
       else
-        counter_value := value
+        counter_value := value;
+      counter_history.(!history) <- value;
+      incr history
+    end
   in
   let callbacks = Callbacks.create
     ~runtime_counter
@@ -137,8 +156,6 @@ let run_and_compare ~__LINE__ f counter_expected counter_type observe_gc =
   let observe () =
     (* major GC counters are only emitted if a major slice occurred *)
     let _ : int = Gc.major_slice 0 in
-    (* polling allocates int64 timestamps, so flush any events here where both can see them *)
-    poll cursor callbacks;
     (* ensure stats are up-to-date for [quick_stat] *)
     Gc.minor ();
     let gc = Gc.quick_stat () |> observe_gc in
@@ -154,23 +171,26 @@ let run_and_compare ~__LINE__ f counter_expected counter_type observe_gc =
   let after_gc, after_counter = observe () in
   let delta_gc = after_gc - baseline_gc
   and delta_counter = after_counter - baseline_counter in
-  delta_gc, delta_counter
+  delta_gc, delta_counter, Array.sub counter_history 0 !history |> Array.to_seq |> Seq.filter (fun x -> x > 0) |> Array.of_seq
+
+let gc_stat_words = Gc.quick_stat () |> Obj.repr |> Obj.reachable_words
 
 let run_and_compare_test ~__LINE__ f counter_expected counter_type observe_gc expected_value_geq=
-  let delta_gc0, delta_counter0 = run_and_compare ~__LINE__:Stdlib.__LINE__ ignore counter_expected counter_type observe_gc in
+  let delta_gc0, delta_counter0, history0 = run_and_compare ~__LINE__:Stdlib.__LINE__ ignore counter_expected counter_type observe_gc in
   let tol = abs (delta_gc0 - delta_counter0) in
   if tol > delta_gc0 then begin
-    Dump.dump ();
+    Dump.dump counter_expected counter_type history0;
     Format.eprintf "[!] Runtime counter value %s on no-op workload: %+d, GC counter value: %+d@."
       (runtime_counter_name counter_expected)
       delta_counter0 delta_gc0;
   end;
-  let tol = max 10 (max delta_gc0 tol) in (* don't be too strict *)
-  let delta_gc, delta_counter = run_and_compare ~__LINE__ f counter_expected counter_type observe_gc in
+  (* poll runs after quick_stat, so it may see some words promoted *)
+  let tol = max gc_stat_words tol in (* don't be too strict *)
+  let delta_gc, delta_counter, history = run_and_compare ~__LINE__ f counter_expected counter_type observe_gc in
   let bad = ref false in
   let log fmt =
     bad := true;
-    Dump.dump ();
+    Dump.dump counter_expected counter_type history;
     let finish ppf = Format.fprintf ppf "@." in
     Format.eprintf "[!] ";
     Format.kfprintf finish Format.err_formatter fmt
@@ -225,15 +245,13 @@ let run_with_spin_domain ~do_major_slice f () =
   in
   run_with_domain ~domain_workload ~after ~before f
 
-let run_domains ~do_major_slice n f () =
-  let finally () =
-    if do_major_slice then major_slice ()
-  in
+let run_domains ~after n f () =
   let domains = Array.init n @@ fun _ ->
     Domain.spawn @@ fun () ->
-    Fun.protect ~finally f
+    Fun.protect ~finally:after f
   in
   Array.iter Domain.join domains
+
 
 let run_and_compare_scenarios f counter_expected counter_type
 observe_gc expected_value_geq=
@@ -255,17 +273,16 @@ observe_gc expected_value_geq=
   scenario ~__LINE__ "Main domain, with spinning extra domain and no explicit major slice"
     (run_with_spin_domain ~do_major_slice:false f);
 
-  scenario ~__LINE__ "2 domains, no explicit major slice"
-    (run_domains 2 ~do_major_slice:false f);
+  [2; 8] |> List.iter @@ fun ndomains ->
+  let name s = Printf.sprintf "%d domains, %s" ndomains s in
+  scenario ~__LINE__ (name "no explicit major slice")
+    (run_domains ndomains ~after:ignore f);
 
-  scenario ~__LINE__ "2 domains, with explicit major slice"
-    (run_domains 2 ~do_major_slice:true f);
+  scenario ~__LINE__ (name "with explicit major slice")
+    (run_domains ndomains ~after:ignore f);
 
-  scenario ~__LINE__ "8 domains, no explicit major slice"
-    (run_domains 8 ~do_major_slice:false f);
-
-  scenario ~__LINE__ "8 domains, with explicit major slice"
-    (run_domains 8 ~do_major_slice:true f)
+  scenario ~__LINE__ (name "with full major GC")
+    (run_domains ndomains ~after:Gc.full_major f)
 
 
 
@@ -274,6 +291,7 @@ let max_young_wosize = 256
 let word_size_bytes = Sys.word_size / 8
 
 let alloc_words words =
+  allocating_words words;
   (* do not use array allocations, because they might also trigger a minor GC *)
   String.make ((words - 2) * word_size_bytes) ' '
 
@@ -293,21 +311,22 @@ let () =
     let counter = EV_C_MAJOR_ALLOCATED_WORDS in
     run_and_compare_scenarios ignore counter Additive (fun t -> t.major_words |> int_of_float) 0;
 
-    let run_alloc ?(promote=false) n =
-      Format.printf "@.Allocation size: %d@." n;
+    let run_alloc ?(promote=false) count n =
+      Format.printf "@.Allocation size: %d, count: %d@." n count;
       run_and_compare_scenarios (fun () ->
         (* ensure that we see the exact value [n] promoted,
            and not summed with previous values in the minor heap
          *)
         if promote then Gc.minor ();
-        let s = alloc_words n in
+        let s = Array.init count (fun _ -> alloc_words n) in
         if promote then Gc.minor ();
         (* keep [s] alive across Gc.minor to ensure promotion *)
         Sys.opaque_identity s |> ignore
       ) counter Additive (fun t -> t.major_words |> int_of_float) n
     in
 
-    run_alloc distinguishable_large;
+    run_alloc 1 distinguishable_large;
 
-    run_alloc ~promote:true distinguishable_small
+    run_alloc ~promote:true 1 distinguishable_small;
+
 
